d1c113add233b19eb18ac1646710d3ff40368d6f
anurag akella <78042808+akellaanurag@users.noreply.github.com>
2022-10-19 22:16:13 +0200

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.header('🥣 🥗 🐔 🥑🍞Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
